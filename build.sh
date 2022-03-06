fc358f925c931e1310f7dfe2806e6833de73b1b1
HereticWay <vpleft@gmail.com>
2022-03-06 18:41:41 +0100

    improve build.sh;

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-docker build -t lavalink-rpi .
+LONG_VERSION="1.0"
+SHORT_VERSION="1"
 
+docker build -t lavalink-rpi .
+docker tag lavalink-rpi vpleft/lavalink-rpi:latest
+docker tag lavalink-rpi vpleft/lavalink-rpi:$LONG_VERSION
+docker tag lavalink-rpi vpleft/lavalink-rpi:$SHORT_VERSION
